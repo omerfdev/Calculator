5b609c21bfaa8fe1bb227932ede2d925f2eced96
omeralmali1 <omeralmali1@gmail.com>
2023-04-19 23:47:56 +0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿bool alwaysRun = true;
+while (alwaysRun)
+{
+    Console.WriteLine("Welcome Calculate Program");
+    int numberx = 0; int numbery = 0;
+    Console.WriteLine("Please Enter For The Process You Want To Do : Collect + , Extraction - , Hitting *, Divide / ");
+    char process = Convert.ToChar(Console.ReadLine());
+    switch (process)
+    {
+        case '+':
+            Console.WriteLine("Please Enter Number");
+            numberx = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Number");
+            numbery = int.Parse(Console.ReadLine());
+            Console.WriteLine("Result=" + " " + (numberx + numbery));
+            Console.ReadLine();
+            break;
+        case '-':
+            Console.WriteLine("Please Enter Number");
+            numberx = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Number");
+            numbery = int.Parse(Console.ReadLine());
+            Console.WriteLine("Result=" + " " + (numberx - numbery));
+            Console.ReadLine();
+            break;
+        case '*':
+            Console.WriteLine("Please Enter Number");
+            numberx = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Number");
+            numbery = int.Parse(Console.ReadLine());
+            Console.WriteLine("Result=" + " " + (numberx * numbery));
+            Console.ReadLine();
+            break;
+        case '/':
+            Console.WriteLine("Please Enter Number");
+            numberx = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Number");
+            numbery = int.Parse(Console.ReadLine());
+            if (numbery == 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Divide by zero exception");
+                Console.WriteLine("Please Enter Number");
+                numbery = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("Result=" + " " + (numberx / numbery));
+            Console.ReadLine();
+            break;
+        default:
+            Console.WriteLine("Unhandled Process ");
+
+            break;
+
+    }
+
+}
+
+
+
